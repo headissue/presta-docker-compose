e5f1eb13b4e5dbae4cea817be5f57b1c8e9bf8b8
Jens Wilke <jw_github@headissue.com>
2023-02-22 18:29:55 +0800

    fix and restructure rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,94 +1,177 @@
 #!/bin/bash
 
-set -x
+set -xe
+
+cd "$(dirname "$0")"
+export DOWNLOADS="`pwd`/Downloads"
+export SHOP_DIR="`pwd`/shop1_data/html"
+export STARTUP_TIMEOUT=1000;
+export BINDADDRESS=localhost;
+export PORT=8881;
 
 function stop_services() {
   docker-compose down
 }
 
-function cd_to_project_dir() {
-  cd "$(dirname "$0")" || exit 1
-}
-
 function clean_volumes() {
-  cd_to_project_dir
-  cd mariadb_data || exit 1
-  sudo git clean -Xdf
-  cd ../shop1_data || exit 1
-  sudo git clean -Xdf
+  ( cd mariadb_data; sudo git clean -Xdf )
+  ( cd shop1_data; sudo git clean -Xdf )
 }
 
-function get_from_github_release_zip() {
-  url=$1
-  vendor=$2
+# Common https download. Downloads a file if not existing already in $DOWNLOADS
+#
+# dl <url> <localfile> <sha256sum>
+#
+dl() {
+mkdir -p $DOWNLOADS
+local file="$DOWNLOADS/$2"
+local mirrorpath="`echo "$1" | sed -r -e 's#^https?://##'`"
+test -f "$file" || {
+  # TODO: have a download mirror
+  # curl -s -L -f "http://dump.headissue.net/dump/mirror/${mirrorpath}" -o "$file" || \
+  curl -s -L -f -b "oraclelicense=accept-securebackup-cookie" "$1" -o "$file" || {
+    rm -f "$file"
+    echo "Download failed" 1>&2;
+    return 1;
+  }
+}
+sum=`sha256sum < $file | awk '{print $1;}'`;
+if [ "$3" = 'todo' ]; then
+  echo `basename "$file"` $sum;
+  return;
+fi  
+sum=`sha256sum < $file | awk '{print $1;}'`;
+if [ "$3" != "$sum" ]; then
+  echo "sha256sum does not match for $1" 1>&2
+  rm -f "$file"
+  return 1;
+fi
+}
+
+#
+# <vendor> <project> <version> <sha256sum>
+#
+fetch_githup_release_zip() {
+  vendor=$1
+  export project=$2
   version=$3
-  if test ! -d vendor/$vendor-$version; then
-    mkdir -p vendor/$vendor
-    wget $url -O vendor/$vendor/$version.zip
-    unzip vendor/$vendor/$version.zip -d vendor/$vendor-$version
-  fi
+  sha256sum=$4
+  local fn=$1-$2-$3.zip
+  dl https://github.com/$vendor/$project/releases/download/v$version/$project.zip $fn $sha256sum
+  export file="$fn";
 }
 
-function get_dependencies() {
-  get_from_github_release_zip \
-    "https://github.com/Oksydan/falcon/releases/download/v3.0.1/falcon.zip" \
-    "Oksydan/falcon" \
-    "3.0.1"
-
-  get_from_github_release_zip \
-    "https://github.com/Oksydan/is_themecore/releases/download/v3.0.1/is_themecore.zip" \
-    "Oksydan/is_themecore" \
-    "3.0.1"
-
-  get_from_github_release_zip \
-    "https://github.com/Oksydan/is_imageslider/releases/download/v2.0.1/is_imageslider.zip" \
-    "Oksydan/is_imageslider" \
-    "2.0.1"
-
-  get_from_github_release_zip \
-    "https://github.com/Oksydan/is_searchbar/releases/download/v2.0.0/is_searchbar.zip" \
-    "Oksydan/is_searchbar" \
-    "2.0.0"
-
-  get_from_github_release_zip \
-    "https://github.com/Oksydan/is_shoppingcart/releases/download/v2.0.0/is_shoppingcart.zip" \
-    "Oksydan/is_shoppingcart" \
-    "2.0.0"
+#
+# Replaces existing directory drops cruft files from theme like MACOS...
+#
+_unpack_project() {
+local dir=unpack-$$-$project;
+    mkdir -p $dir;
+    unzip -q $DOWNLOADS/"$file" -d $dir;
+    test -d $dir/$project || {
+      echo "$file: directory with $project expected in zip";
+      exit 1;
+    }
+    if test -d $project; then
+      mv $project $project.backup;
+    fi  
+    mv $dir/$project $project
+    rm -rf $project.backup
+    rm -rf $dir
 }
 
-function start_services() {
-  docker-compose up -d --build &
-  url="http://localhost:8881"
-  http_status=""
-  while [[ "$http_status" != "200" ]]; do
-    wget_output=$(wget --spider --server-response "$url" 2>&1)
-    http_status=$(echo "$wget_output" | awk '/HTTP\/1.1/{print $2}')
-    sleep 1
-  done
+# Install module zip from $DOWNLOADS folder
+# Takes argument from $file
+#
+install_module_zip() {
+  (  
+    cd $SHOP_DIR/modules;
+    _unpack_project;
+  )
 }
 
-function set_permissions() {
-  cd_to_project_dir
-  sudo chmod -R a+w ./shop1_data
+# Install module zip from $DOWNLOADS folder
+# Takes argument from $file
+#
+install_theme_zip() {
+  (  
+    cd $SHOP_DIR/themes;
+    _unpack_project;
+  )
 }
 
-function copy_modules() {
-  cp -r custom_modules/* shop1_data/html/modules
-  cp -r vendor/Oksydan/is_*/is_* shop1_data/html/modules
-  sudo chown -R www-data:www-data shop1_data/html/modules
+install_falcon_theme() {
+fetch_githup_release_zip Oksydan is_themecore 3.0.1 todo
+install_module_zip
+fetch_githup_release_zip Oksydan is_imageslider 2.0.1 todo
+install_module_zip
+fetch_githup_release_zip Oksydan is_searchbar 2.0.0 todo
+install_module_zip
+fetch_githup_release_zip Oksydan is_shoppingcart 2.0.0 todo
+install_module_zip
+fetch_githup_release_zip Oksydan falcon 3.0.1 todo
+install_theme_zip
 }
 
-function enable_theme() {
-  cp -r vendor/Oksydan/falcon-3.0.2 shop1_data/html/themes/falcon
-  sudo chown -R www-data:www-data shop1_data/html/themes
+function enable_falcon-theme() {
   docker exec -it shop1 \
     runuser -u www-data php bin/console prestashop:theme:enable falcon
 }
 
+# Wait until application has started
+#
+# Variables:
+# TIMEOUT: don't wait long than TIMEOUT seconds
+# PORT: http port tomcat listens to
+#
+# Return value:
+# 0: started successful
+# 1: error detected
+#
+waitForStartup() {
+  test -z "$PORT" && return;
+  local starttime=`date +%s`;
+  local maxtime=$(( $starttime + $STARTUP_TIMEOUT ));
+  local rc;
+  while [ `date +%s` -lt $maxtime ]; do
+    rc=0;
+    msg=`curl --max-time 5 http://$BINDADDRESS:$PORT -o /dev/null 2>&1` || rc=$?;
+    case $rc in
+      # we got an ansewer, tomcat is up
+      0) return;;
+      # empty reply from server
+      52) sleep 3; continue;;
+      # connection reset by peer
+      56) sleep 3; continue;;
+      # could not connect (nobody listens on port)
+      7) sleep 3; continue;;
+      # timeout
+      28)continue;;
+      *) echo "$msg" 1>&2;
+         return 1;;
+    esac
+  done
+}
+
+function start_services() {
+  docker-compose up -d --build &
+  waitForStartup
+}
+
+function set_permissions() {
+  sudo chmod -R a+w ./shop1_data
+}
+
+all() {
 stop_services
 clean_volumes
 start_services
 set_permissions
-get_dependencies
-copy_modules
-enable_theme
+install_falcon_theme
+set_permissions
+enable_falcon-theme
+}
+
+# "$@"
+
+all
